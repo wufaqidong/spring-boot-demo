09f6ea1b475bb3e376756191f30d08e68bdde60e
xbinbin <xb8023#XB>
2022-06-15 13:13:11 +0800

    基础知识

--- src/main/java/com/example/springbootdemo/SpringBootDemoApplication.java
+++ src/main/java/com/example/springbootdemo/SpringBootDemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootDemoApplication {
 
+    // 启动类
     public static void main(String[] args) {
         SpringApplication.run(SpringBootDemoApplication.class, args);
     }
